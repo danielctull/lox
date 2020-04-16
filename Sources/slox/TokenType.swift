fedc0720d8956f2558340749497a5d3ae03764f6
Daniel Tull <dt@danieltull.co.uk>
2020-04-16 21:01:16 +0100

    (4.2.1) Add token type

---
+++ Sources/slox/TokenType.swift
@@ -0,0 +1,51 @@
+
+enum TokenType {
+
+    // Single-character tokens
+    case leftParenthesis
+    case rightParenthesis
+    case leftBrace
+    case rightBrace
+    case comma
+    case dot
+    case minus
+    case plus
+    case semicolon
+    case slash
+    case star
+
+    // One or two character tokens
+    case bang
+    case bangEqual
+    case equal
+    case equalEqual
+    case greater
+    case greaterEqual
+    case less
+    case lessEqual
+
+    // Literals
+    case identifier
+    case string
+    case number
+
+    // Keywords
+    case and
+    case `class`
+    case `else`
+    case `false`
+    case fun
+    case `for`
+    case `if`
+    case `nil`
+    case or
+    case print
+    case `return`
+    case `super`
+    case this
+    case `true`
+    case `var`
+    case `while`
+
+    case eof
+}
