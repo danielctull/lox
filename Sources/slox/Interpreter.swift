636783104572a7458c9058ce48a97ca057a308e3
Daniel Tull <dt@danieltull.co.uk>
2020-05-16 21:43:03 +0100

    Move the conformance to its own file
    This is to match with Expression and Statement.

--- Sources/slox/Interpreter.swift
+++ Sources/slox/Interpreter.swift
@@ -229,19 +229,6 @@ extension Value {
     }
 }
 
-extension Value: CustomStringConvertible {
-
-    public var description: String {
-        switch self {
-        case let .boolean(value): return value.description
-        case let .number(value): return value.description
-        case let .string(value): return value
-        case let .callable(value): return value.description
-        case .nil: return "nil"
-        }
-    }
-}
-
 extension Value {
 
     fileprivate var typeName: String {
